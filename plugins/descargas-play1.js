141fcf34cda0fecd34f8867fb5bd86bc31bbe1b3
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-08-23 18:03:59 -0300

    Update descargas-play1.js

--- plugins/descargas-play1.js
+++ plugins/descargas-play1.js
@@ -38,6 +38,7 @@ console.log(`❗❗ᴇʀʀᴏʀ ${usedPrefix + command} ❗❗`)
 console.log(e)
 handler.limit = 0
 }}
+handler.tags = ['descargas']
 handler.command = ['play', 'play2', 'play3', 'play4', 'playt']
 //handler.limit = 3
 handler.register = true 
